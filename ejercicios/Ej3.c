f79c6809d0bf2c8949c7404bf32cf12abf0a2201
Hector009777 <hectormancia46@gmail.com>
2018-09-01 11:36:52 -0600

    Completo

---
+++ ejercicios/Ej3.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+	#include <stdlib.h>
+	
+	int main() {
+     int n=0,menu=0;
+     printf("Ingrese un tamaño\n");
+        scanf("%d",&n);
+         int vector[n];
+        for(int i=0;i<n;i++){
+              vector[i]=0;
+              
+         }
+    
+   
+    
+    do{
+         printf("Vector:");
+         for(int j=0;j<n;j++){
+              printf("[%d] ",vector[j]);
+              
+         }
+         
+         printf("\n");
+         printf("-----------\n");
+         printf("¿Que desea realizar?\n");
+          printf("1.Agregar un dato\n");
+           printf("2.Buscar un valor en el vector\n");
+            printf("3.Editar\n");
+            printf("4.Eliminar\n");
+            printf("-----------\n");
+            scanf("%d",&menu);
+            
+           if(menu==1){
+              int posi=0,dato=0;
+               do{
+           printf("En que posicion desea agregar el dato de posicion 0 a posicion %d \n ",n-1);
+            scanf("%d",&posi);
+               }while(posi>n-1);
+               
+                printf("Ingrese el numero \n ");
+                scanf("%d",&dato);
+                vector[posi]=dato;
+                printf("Hecho"); 
+                printf("\n");
+           } else if(menu==2){
+               int dato=0,enc=0;
+              
+               
+                printf("Ingrese el numero a buscar\n ");
+                scanf("%d",&dato);
+                
+                for(int j=0;j<n;j++){
+                    if(dato==vector[j]){
+                         enc=1;
+                         printf("El numero ingresado esta en la posicion %d \n ",x);
+                    }
+                        
+                 }
+                if(enc==0){
+                        printf("El numero no existe\n");
+                        }
+                    enc=0;
+                        printf("\n");
+           } else if(menu==3){
+               
+             int dato=0,nuevo=0,enc=0;
+                printf("Ingrese el numero a editar\n ");
+                scanf("%d",&dato);
+                for(int j=0;j<n;j++){
+                    if(dato==vector[j]){
+                        enc=1; 
+                        printf("El numero nuevo \n ");
+                         scanf("%d",&nuevo);
+                         vector[j]=nuevo;
+                    }
+                }
+                if(enc==0){
+                        printf("El numero no existe\n");
+                        }
+                 enc=0;   
+               printf("\n");
+               
+           } else if(menu==4){
+               
+               int dato=0,enc=0;
+                printf("Ingrese el numero a eliminar\n ");
+                scanf("%d",&dato);
+                for(int j=0;j<n;j++){
+                    if(dato==vector[j]){
+                        enc=1;
+                        vector[j]=0;
+                         printf("Eliminado!\n ");
+                         
+                    }
+                }
+                 if(enc==0){
+                        printf("El numero no existe\n");
+                        }
+                enc=0;
+               
+           } 
+            
+            printf("\n");
+             printf("-------------\n");
+            
+    }while(menu<=4);
+    
+    
+return (0);
+    
+}
+
---
+++ ejercicios/Ej4.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n=0;
+     printf("Ingrese un tamaño\n");
+        scanf("%d",&n);
+    
+    int matriz[n][n];
+    
+   for(int m=0;m<n;m++){  
+    for(int k=0;k<n;k++){
+        printf("Ingrese un valor\n");
+        scanf("%d",&matriz[m][k]);
+    }
+    }
+    
+    
+    printf("---------");
+      for(int a=0;a<n;a++){  
+                printf("\n"); 
+
+    for(int b=0;b<n;b++){
+        printf(" ");
+        printf("%d",matriz[a][b]);
+    }  
+    
+    }
+    
+    printf("\n");
+    printf("---------");
+     printf("\n");
+    int col=n,sumaD1=0;
+    
+    for(int k=n;k>0;k--){
+         printf(" ");
+       
+         sumaD1=sumaD1+matriz[col-1][k-1];
+        col--;
+    }
+     int col2=0,sumaD2=0;
+    
+    for(int k=n;k>0;k--){
+     
+         sumaD2=sumaD2+matriz[col2][k-1];
+        col2++;
+    }
+     
+     int sumaH=0,fil=0;
+      int sumaV=0,col3=0;
+     
+     
+     for(int k=0;k<n;k++){
+         while(fil<n){
+             sumaH=sumaH+matriz[k][fil];
+             fil++;
+         }
+         
+         printf("Suma horizontal = ");
+         printf("%d",sumaH);
+          printf("\n");
+          fil=0;
+          sumaH=0;
+          
+          while(col3<n){
+             sumaV=sumaV+matriz[col3][k];
+             col3++;
+         }
+         
+         printf("Suma vertical= ");
+         printf("%d",sumaV);
+          printf("\n");
+          col3=0;
+          sumaV=0;
+     }
+     
+     printf("Suma diagonal uno = ");
+      printf("%i",sumaD2);
+      printf("\n");
+     printf("Suma diagonal dos = ");
+      printf("%i",sumaD1); 
+     
+    return 0;
+}
